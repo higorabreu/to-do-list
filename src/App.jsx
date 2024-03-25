7ea870ab7aa091902fd5f808dfd94c40bfe747ce
Higor Abreu <higorabreuf@gmail.com>
2024-03-24 23:01:38 -0300

    form add

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Todo from "./components/Todo"
+import "./App.css";
+import TodoForm from './components/TodoForm';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState ([
+    {
+      id:1,
+      text: "criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id:2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id:3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo = (text, category) => {
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false,
+    }, 
+  ];
+
+  setTodos(newTodos);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div className="app">
+    <h1>Lista de Tarefas</h1>
+    <div className='todo-list'>
+      {todos.map((todo) => (
+        <Todo key={todo.id} todo={todo} />
+      ))}
+    </div>
+    <TodoForm addTodo={addTodo} />
+  </div>  
   )
 }
 
