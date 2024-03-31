0a8124f9acf0e93fb0e5c0d3c39cce315b765c2d
Higor Abreu <higorabreuf@gmail.com>
2024-03-31 18:34:23 -0300

    filter add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Todo from "./components/Todo"
 import TodoForm from './components/TodoForm';
 import Search from './components/Search';
+import Filter from './components/Filter';
 
 function App() {
   const [todos, setTodos] = useState ([
@@ -27,6 +28,9 @@ function App() {
   ]);
 
   const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [...todos, {
@@ -58,8 +62,22 @@ function App() {
   <div className="app">
     <h1>Lista de Tarefas</h1>
     <Search search={search} setSearch={setSearch}/>
+    <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
     <div className='todo-list'>
       {todos
+        .filter((todo) => 
+          filter === "All" 
+            ? true 
+            : filter === "Completed" 
+            ? todo.isCompleted 
+            : !todo.isCompleted
+      )
+      .sort((a, b) =>
+        sort === "Asc"
+        ? a.text.localeCompare(b.text)
+        : b.text.localeCompare(a.text)
+
+      )
       .filter((todo) =>
       todo.text.toLowerCase().includes(search.toLowerCase())
       )
