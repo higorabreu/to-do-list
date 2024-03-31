7d49e63f50d5e6ae6002967be6ecf3ae0e0148e6
Higor Abreu <higorabreuf@gmail.com>
2024-03-31 16:56:13 -0300

    style update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [todos, setTodos] = useState ([
     {
       id:1,
-      text: "criar funcionalidade x no sistema",
+      text: "Criar funcionalidade x no sistema",
       category: "Trabalho",
       isCompleted: false,
     },
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,8 +4,8 @@ const Todo = ({ todo }) => {
   return (
       <div className='todo'>
         <div className='content'>
-        <p>({todo.text})</p>
-        <p className='category'>{todo.category}</p>
+        <p>{todo.text}</p>
+        <p className='category'>({todo.category})</p>
         </div>
         <div>
           <button className='complete'>Completar</button>
